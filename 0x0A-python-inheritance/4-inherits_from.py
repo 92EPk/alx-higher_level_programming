faf99417fac28ddb5c107db1192f4f5e3ae95729
Bx <53805165+92EPk@users.noreply.github.com>
2024-04-17 01:42:13 +0200

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Module 4-inherits_from.
-Finds if the object is an instance of a class that inherited
-(directly or indirectly) from the specified class."""
+''' mudol 2-is_same_class.py'''
 
-def inherits_from(obj, a_class):
-    """Determines if obj is an instance of a class."""
-    
-    return isinstance(obj, a_class) and type(obj) != a_class
+def is_same_class(obj, a_class):
+    '''is_same_class function'''
+    print(type(obj).__name__, a_class.__name__)
+    return isinstance(obj, a_class)
