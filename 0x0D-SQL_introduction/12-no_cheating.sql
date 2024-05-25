9e9c69b27bc5c746e405629e79cf3423ac916bc8
Bx <53805165+92EPk@users.noreply.github.com>
2024-05-25 06:23:24 +0300

    Update 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
--- updates teh score of Bob to 10 int the 
--- second table
--- using only the name field
+-- Updates the score of Bob to 10 in the table second_table
 
-UPDATE second_table SET score = 10 WHERE name = 'Bob';
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
