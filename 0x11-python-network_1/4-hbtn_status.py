35b7127f7ca5a6a64ceccf10618b515f490114bf
92EPK <b9x@outlook.com>
2024-07-31 16:56:12 -0700

    Tasks Solutions

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Script sends an HTTP Request to a URL and prinyt out the response
+"""
+
+import requests
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    the_text = req.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(the_text)))
+    print('\t- content: {}'.format(the_text))
